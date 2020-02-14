71ff61f92f50d4064e230e271523ee2a2de4c91d
Chris Gyurgyik <37983775+cgyurgyik@users.noreply.github.com>
2020-02-13 19:08:32 -0500

    Add example.

---
+++ polarCoordinateTraversalExample.m
@@ -0,0 +1,19 @@
+function polarCoordinateTraversalExample
+    min_bound = [0.0, 0.0];
+    max_bound = [30.0, 30.0];
+    
+    ray_origin = [5.0, 5.0];
+    ray_direction = [1.0, 1.0];
+    
+    circle_center = [15.0, 15.0];
+    circle_max_radius = 10.0;
+    num_radial_sections = 5;
+    num_angular_sections = 4;
+    
+    t_begin = 0.0;
+    t_end = 15.0;
+    
+    verbose = true;
+    
+    polarCoordinateTraversal(min_bound, max_bound, ray_origin, ray_direction, circle_center, circle_max_radius, num_radial_sections, num_angular_sections, t_begin, t_end, verbose);
+end
