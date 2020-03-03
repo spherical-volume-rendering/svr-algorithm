bcfed3fa34f112040e17891521c2b08b0082d824
Youhan Yuan <youhan@dhcp-hol-5055.eduroam.cornell.edu>
2020-03-03 14:13:49 -0500

    fix odd angular section test

--- polarCoordinateTraversalTest.m
+++ polarCoordinateTraversalTest.m
@@ -444,20 +444,20 @@ end
 function testRayWithOddNumAngularSections(testCase):
     min_bound = [0.0, 0.0];
     max_bound = [30.0, 30.0];
-    ray_origin = [6.0, 16.0];
-    ray_direction = [-1.0, -3.0];
+    ray_origin = [5.0, 10.0];
+    ray_direction = [3.0, 2.0];
     circle_center = [15.0, 15.0];
     circle_max_radius = 10.0;
     num_radial_sections = 3;
-    num_angular_sections = 4;
+    num_angular_sections = 3;
     t_begin = 0.0;
     t_end = 30.0;
     verbose = false;
     
     [rVoxels, thetaVoxels] = polarCoordinateTraversal(min_bound, max_bound, ray_origin, ray_direction, ...
         circle_center, circle_max_radius, num_radial_sections, num_angular_sections, t_begin, t_end, verbose);
-        expected_rVoxels     = [1];
-        expected_thetaVoxels = [2];
+        expected_rVoxels     = [1,2,3,3,2,1];
+        expected_thetaVoxels = [1,1,1,0,0,0];
         
         verifyEqual(testCase, rVoxels, expected_rVoxels);
         verifyEqual(testCase, thetaVoxels, expected_thetaVoxels);
