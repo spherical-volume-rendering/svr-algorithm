4c1770d3808cde25bfe221d7f2c0e5431eb756c8
Chris Gyurgyik <gyurgyikcp@gmail.com>
2020-03-23 16:52:18 -0400

    Untracked files issue resolved to fix .gitignore.

--- cpp/testing/cmake-build-debug/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/cgyurgyik/CLionProjects/spherical-volume-rendering/cpp/testing/googletest/googlemock
-# Build directory: /Users/cgyurgyik/CLionProjects/spherical-volume-rendering/cpp/testing/cmake-build-debug/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- cpp/testing/cmake-build-debug/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/cgyurgyik/CLionProjects/spherical-volume-rendering/cpp/testing/googletest/googletest
-# Build directory: /Users/cgyurgyik/CLionProjects/spherical-volume-rendering/cpp/testing/cmake-build-debug/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
