c46862d558013400485a42650fdaf989921a230e
Chris Gyurgyik <gyurgyikcp@gmail.com>
2020-03-30 10:28:27 -0400

    Add sphere at origin test.

--- cpp/testing/spherical_volume_rendering_test.cpp
+++ cpp/testing/spherical_volume_rendering_test.cpp
@@ -10,7 +10,27 @@
 //
 // For information on Google Test, see: https://github.com/google/googletest/blob/master/googletest/README.md
 // For examples of Google Test, see: https://github.com/google/googletest/tree/master/googletest/samples
+
 namespace {
+    // Determines equality amongst actual voxels, and the expected voxels.
+    void expectEqualVoxels(const std::vector<SphericalVoxel>& actual_voxels,
+                           const std::vector<size_t>& expected_radial_voxels,
+                           const std::vector<size_t>& expected_theta_voxels,
+                           const std::vector<size_t>& expected_phi_voxels) {
+
+        EXPECT_EQ(actual_voxels.size(), expected_radial_voxels.size());
+        EXPECT_EQ(actual_voxels.size(), expected_theta_voxels.size());
+        EXPECT_EQ(actual_voxels.size(), expected_phi_voxels.size());
+
+        int i = 0;
+        for (const auto voxel : actual_voxels) {
+            EXPECT_EQ(voxel.radial_voxel, expected_radial_voxels[i]);
+            EXPECT_EQ(voxel.angular_voxel, expected_theta_voxels[i]);
+            EXPECT_EQ(voxel.azimuthal_voxel, expected_phi_voxels[i]);
+            ++i;
+        }
+    }
+
     // The tolerance provided for the floating point error carried during the
     // spherical coordinate traversal algorithm.
     constexpr double tolerance = 10e-20;
@@ -32,7 +52,31 @@ namespace {
 
         const double t_begin = 0.0;
         const double t_end = 15.0;
-        const auto voxels = sphericalCoordinateVoxelTraversal(ray, grid, t_begin, t_end, tolerance);
-        EXPECT_EQ(voxels.size(), 0);
+        const auto actual_voxels = sphericalCoordinateVoxelTraversal(ray, grid, t_begin, t_end, tolerance);
+        EXPECT_EQ(actual_voxels.size(), 0);
+    }
+
+    TEST(SphericalCoordinateTraversal, SphereCenteredAtOrigin) {
+        const BoundVec3 min_bound(-20.0, -20.0, -20.0);
+        const BoundVec3 max_bound(20.0, 20.0, 20.0);
+        const BoundVec3 sphere_center(0.0, 0.0, 0.0);
+        const double sphere_max_radius = 10.0;
+        const size_t num_radial_sections = 4;
+        const size_t num_angular_sections = 4;
+        const size_t num_azimuthal_sections = 4;
+        const SphericalVoxelGrid grid(min_bound, max_bound, num_radial_sections,
+                                      num_angular_sections,
+                                      num_azimuthal_sections, sphere_center, sphere_max_radius);
+        const BoundVec3 ray_origin(-13.0, -13.0, -13.0);
+        const UnitVec3 ray_direction(1.0, 1.0, 1.0);
+        const Ray ray(ray_origin, ray_direction);
+        const double t_begin = 0.0;
+        const double t_end = 30.0;
+
+        const auto actual_voxels = sphericalCoordinateVoxelTraversal(ray, grid, t_begin, t_end, tolerance);
+        const std::vector<size_t> expected_radial_voxels = {1,2,3,4,4,3,2,1};
+        const std::vector<size_t> expected_theta_voxels = {2,2,2,2,0,0,0,0};
+        const std::vector<size_t> expected_phi_voxels = {2,2,2,2,0,0,0,0};
+        expectEqualVoxels(actual_voxels, expected_radial_voxels, expected_theta_voxels, expected_phi_voxels);
     }
 }
