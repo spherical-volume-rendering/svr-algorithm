32003cc5408c92a746e937cf9ae3d91ba7a54fd3
Chris Gyurgyik <37983775+cgyurgyik@users.noreply.github.com>
2020-04-07 14:30:02 -0400

    Update sphericalCoordinateTraversalTest.m

--- matlab/spherical-traversal/sphericalCoordinateTraversalTest.m
+++ matlab/spherical-traversal/sphericalCoordinateTraversalTest.m
@@ -54,3 +54,26 @@ function testSphereCenteredAtOrigin(testCase)
     verifyEqual(testCase, thetaVoxels, [2,2,2,2,0,0,0,0]);
     verifyEqual(testCase, phiVoxels, [2,2,2,2,0,0,0,0]);
 end
+
+% Change direction of ray slightly in the XY plane.
+function testRayChangesinXYPlane(testCase)
+    min_bound = [-20, -20.0, -20.0];
+    max_bound = [20.0, 20.0, 20.0];
+    ray_origin = [-13.0, -13.0, -13.0];
+    ray_direction = [1.0, 1.5, 1.0];
+    sphere_center = [0.0, 0.0, 0.0];
+    sphere_max_radius = 10.0;
+    
+    num_radial_sections = 4;
+    num_angular_sections = 4;
+    num_azimuthal_sections = 4;
+    t_begin = 0.0;
+    t_end = 30.0;
+    verbose = false;
+    
+    [rVoxels, thetaVoxels, phiVoxels] = sphericalCoordinateTraversal(min_bound, max_bound, ray_origin, ray_direction, ...
+    sphere_center, sphere_max_radius, num_radial_sections, num_angular_sections, num_azimuthal_sections, t_begin, t_end, verbose);
+    verifyEqual(testCase, rVoxels, [1,2,2,3,2,2,2,1]);
+    verifyEqual(testCase, thetaVoxels, [2,2,1,1,1,1,0,0]);
+    verifyEqual(testCase, phiVoxels, [2,2,2,2,2,0,0,0]);
+end
