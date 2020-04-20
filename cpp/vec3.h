9bc4e3617c4be09915bd4c0a344d57adf109c29f
Chris Gyurgyik <gyurgyikcp@gmail.com>
2020-04-20 14:46:45 -0400

    Add noexcept.

--- cpp/vec3.h
+++ cpp/vec3.h
@@ -12,26 +12,25 @@
 //      [z]
 struct Vec3 {
 public:
-    Vec3(const double x, const double y, const double z)
-            : x_(x), y_(y), z_(z) {}
+    inline Vec3(const double x, const double y, const double z) : x_(x), y_(y), z_(z) {}
 
-    inline double x() const { return this->x_; }
+    inline double x() const noexcept { return this->x_; }
 
-    inline double y() const { return this->y_; }
+    inline double y() const noexcept { return this->y_; }
 
-    inline double z() const { return this->z_; }
+    inline double z() const noexcept { return this->z_; }
 
-    inline double &x() { return this->x_; }
+    inline double &x() noexcept { return this->x_; }
 
-    inline double &y() { return this->y_; }
+    inline double &y() noexcept { return this->y_; }
 
-    inline double &z() { return this->z_; }
+    inline double &z() noexcept { return this->z_; }
 
-    inline double length() const {
+    inline double length() const noexcept {
         return std::sqrt(this->x() * this->x() + this->y() * this->y() + this->z() * this->z());
     }
 
-    inline double squared_length() const {
+    inline double squared_length() const noexcept {
         return x() * x() + y() * y() + z() * z();
     }
 
@@ -51,36 +50,36 @@ private:
 //                  Langr, J. "Modern C++ Programming with Test-Driven Development: Code Better, Sleep Better" [5.10]
 struct FreeVec3 : Vec3 {
 
-    explicit FreeVec3(const Vec3 &vec3) : Vec3(vec3.x(), vec3.y(), vec3.z()) {}
+    inline explicit FreeVec3(const Vec3 &vec3) : Vec3(vec3.x(), vec3.y(), vec3.z()) {}
 
-    explicit FreeVec3(double x, double y, double z) : Vec3(x, y, z) {}
+    inline explicit FreeVec3(double x, double y, double z) : Vec3(x, y, z) {}
 
-    inline double dot(const Vec3 &other) const {
+    inline double dot(const Vec3 &other) const noexcept {
         return this->x() * other.x() + this->y() * other.y() + this->z() * other.z();
     }
 
-    inline FreeVec3 &operator+=(const FreeVec3 &other) {
+    inline FreeVec3 &operator+=(const FreeVec3 &other) noexcept {
         this->x() += other.x();
         this->y() += other.y();
         this->z() += other.z();
         return *this;
     }
 
-    inline FreeVec3 &operator-=(const FreeVec3 &other) {
+    inline FreeVec3 &operator-=(const FreeVec3 &other) noexcept {
         this->x() -= other.x();
         this->y() -= other.y();
         this->z() -= other.z();
         return *this;
     }
 
-    inline FreeVec3 &operator*=(const double scalar) {
+    inline FreeVec3 &operator*=(const double scalar) noexcept {
         this->x() *= scalar;
         this->y() *= scalar;
         this->z() *= scalar;
         return *this;
     }
 
-    inline FreeVec3 &operator/=(const double scalar) {
+    inline FreeVec3 &operator/=(const double scalar) noexcept {
         assert(scalar != 0.0);
         this->x() /= scalar;
         this->y() /= scalar;
@@ -89,60 +88,60 @@ struct FreeVec3 : Vec3 {
     }
 };
 
-inline FreeVec3 operator+(const FreeVec3 &v) { return v; }
+inline FreeVec3 operator+(const FreeVec3 &v) noexcept { return v; }
 
-inline FreeVec3 operator-(const FreeVec3 &v) {
+inline FreeVec3 operator-(const FreeVec3 &v) noexcept {
     return FreeVec3(-v.x(), -v.y(), -v.z());
 }
 
-inline FreeVec3 operator+(FreeVec3 v1, const FreeVec3 &v2) {
+inline FreeVec3 operator+(FreeVec3 v1, const FreeVec3 &v2) noexcept {
     return v1 += v2;
 }
 
-inline FreeVec3 operator-(FreeVec3 v1, const FreeVec3 &v2) {
+inline FreeVec3 operator-(FreeVec3 v1, const FreeVec3 &v2) noexcept {
     return v1 -= v2;
 }
 
-inline FreeVec3 operator*(FreeVec3 v, const double scalar) {
+inline FreeVec3 operator*(FreeVec3 v, const double scalar) noexcept {
     return v *= scalar;
 }
 
-inline FreeVec3 operator/(FreeVec3 v, const double scalar) {
+inline FreeVec3 operator/(FreeVec3 v, const double scalar) noexcept {
     return v /= scalar;
 }
 
 // A 3-dimensional bounded vector has a fixed start and end point. It represents a fixed point
 // in space, relative to some frame of reference.
 struct BoundVec3 : Vec3 {
-    explicit BoundVec3(const Vec3 &vec3) : Vec3(vec3.x(), vec3.y(), vec3.z()) {}
+    inline explicit BoundVec3(const Vec3 &vec3) : Vec3(vec3.x(), vec3.y(), vec3.z()) {}
 
-    explicit BoundVec3(double x, double y, double z) : Vec3(x, y, z) {}
+    inline explicit BoundVec3(double x, double y, double z) : Vec3(x, y, z) {}
 
-    inline double dot(const Vec3 &other) const {
+    inline double dot(const Vec3 &other) const noexcept {
         return this->x() * other.x() + this->y() * other.y() + this->z() * other.z();
     }
 
-    inline BoundVec3 &operator+=(const FreeVec3 &other) {
+    inline BoundVec3 &operator+=(const FreeVec3 &other) noexcept {
         this->x() += other.x();
         this->y() += other.y();
         this->z() += other.z();
         return *this;
     }
 
-    inline BoundVec3 &operator-=(const FreeVec3 &other) {
+    inline BoundVec3 &operator-=(const FreeVec3 &other) noexcept {
         return *this += (-other);
     }
 };
 
-inline FreeVec3 operator-(const BoundVec3 &v1, const BoundVec3 &v2) {
+inline FreeVec3 operator-(const BoundVec3 &v1, const BoundVec3 &v2) noexcept {
     return FreeVec3(v1.x() - v2.x(), v1.y() - v2.y(), v1.z() - v2.z());
 }
 
-inline BoundVec3 operator+(BoundVec3 v1, const FreeVec3 &v2) {
+inline BoundVec3 operator+(BoundVec3 v1, const FreeVec3 &v2) noexcept {
     return v1 += v2;
 }
 
-inline BoundVec3 operator-(BoundVec3 v1, const FreeVec3 &v2) {
+inline BoundVec3 operator-(BoundVec3 v1, const FreeVec3 &v2) noexcept {
     return v1 -= v2;
 }
 
@@ -150,31 +149,31 @@ inline BoundVec3 operator-(BoundVec3 v1, const FreeVec3 &v2) {
 // a length of 1. To prevent its length from changing, UnitVec3 does not allow
 // for mutations.
 struct UnitVec3 {
-    explicit UnitVec3(double x, double y, double z)
+    inline explicit UnitVec3(double x, double y, double z)
             : UnitVec3(FreeVec3(x, y, z)) {}
 
-    explicit UnitVec3(const Vec3 &vec3) : UnitVec3(FreeVec3(vec3)) {}
+    inline explicit UnitVec3(const Vec3 &vec3) : UnitVec3(FreeVec3(vec3)) {}
 
-    explicit UnitVec3(const FreeVec3 &free_vec3) :
+    inline explicit UnitVec3(const FreeVec3 &free_vec3) :
             inner_(free_vec3 / free_vec3.length()) {}
 
-    inline double x() const { return this->to_free().x(); }
+    inline double x() const noexcept { return this->to_free().x(); }
 
-    inline double y() const { return this->to_free().y(); }
+    inline double y() const noexcept { return this->to_free().y(); }
 
-    inline double z() const { return this->to_free().z(); }
+    inline double z() const noexcept { return this->to_free().z(); }
 
-    inline const FreeVec3 &to_free() const { return inner_; }
+    inline const FreeVec3 &to_free() const noexcept { return inner_; }
 
 private:
     const FreeVec3 inner_;
 };
 
-inline FreeVec3 operator*(const UnitVec3 &v, const double scalar) {
+inline FreeVec3 operator*(const UnitVec3 &v, const double scalar) noexcept {
     return v.to_free() * scalar;
 }
 
-inline FreeVec3 operator/(const UnitVec3 &v, const double scalar) {
+inline FreeVec3 operator/(const UnitVec3 &v, const double scalar) noexcept {
     assert(scalar != 0.0);
     return v.to_free() / scalar;
 }
