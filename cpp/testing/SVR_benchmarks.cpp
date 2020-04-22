b2a39aca5b2827f773b118712f9d47fa07c1efd4
Chris Gyurgyik <gyurgyikcp@gmail.com>
2020-04-22 16:21:29 -0400

    One FPS case.

--- cpp/testing/SVR_benchmarks.cpp
+++ cpp/testing/SVR_benchmarks.cpp
@@ -167,44 +167,47 @@ namespace {
         }
     }
 
-    static void OrthographicRayTrace(benchmark::State &state) {
-        for (auto _ : state) {
-            const BoundVec3 min_bound(-20000.0, -20000.0, -20000.0);
-            const BoundVec3 max_bound(20000.0, 20000.0, 20000.0);
-            const BoundVec3 sphere_center(0.0, 0.0, 0.0);
-            const double sphere_max_radius = 10000.0;
-            const std::size_t num_radial_sections = 10000;
-            const std::size_t num_angular_sections = 10000;
-            const std::size_t num_azimuthal_sections = 10000;
-            const svr::SphericalVoxelGrid grid(min_bound, max_bound, num_radial_sections,
-                                               num_angular_sections,
-                                               num_azimuthal_sections, sphere_center, sphere_max_radius);
-            const double t_begin = 0.0;
-            const double t_end = 100000.0;
+	// 128^3 domain with 256^2 rays in 1 FPS for a scratch paper benchmark.
+	static void OrthographicOneFPS(benchmark::State &state) {
+		for (auto _ : state) {
+			const BoundVec3 min_bound(-200000.0, -200000.0, -200000.0);
+			const BoundVec3 max_bound(200000.0, 200000.0, 20000.0);
+			const BoundVec3 sphere_center(0.0, 0.0, 0.0);
+			const double sphere_max_radius = 1000.0 * 1000.0;
+			const std::size_t num_radial_sections = 128;
+			const std::size_t num_angular_sections = 128;
+			const std::size_t num_azimuthal_sections = 128;
+			const svr::SphericalVoxelGrid grid(min_bound, max_bound, num_radial_sections,
+											   num_angular_sections,
+											   num_azimuthal_sections, sphere_center, sphere_max_radius);
+			const double t_begin = 0.0;
+			const double t_end = 10000.0;
 
-            double ray_origin_x = -5000.0;
-            double ray_origin_y = -5000.0;
-            for (int i = 0; i < 100; ++i) {
-                for (int j = 0; j < 100; ++j) {
-                    const BoundVec3 ray_origin(ray_origin_x, ray_origin_y, 0.0);
-                    const FreeVec3 ray_direction(1.0, 0.0, 0.0);
-                    const Ray ray(ray_origin, ray_direction);
-                    const auto actual_voxels = sphericalCoordinateVoxelTraversal(ray, grid, t_begin, t_end);
-                    ray_origin_y += 100.0;
-                }
-                ray_origin_x += 100.0;
-            }
-        }
-    }
+			double ray_origin_x = -1000.0;
+			double ray_origin_y = -1000.0;
+			const double ray_origin_z = -1100.0;
+		    const double ray_movement = 2000.0 / 256.0;
+			for (int i = 0; i < 256; ++i) {
+				for (int j = 0; j < 256; ++j) {
+				  const BoundVec3 ray_origin(ray_origin_x, ray_origin_y, ray_origin_z);
+				  const FreeVec3 ray_direction(0.0, 0.0, 1.0);
+				  const Ray ray(ray_origin, ray_direction);
+				  const auto actual_voxels = sphericalCoordinateVoxelTraversal(ray, grid, t_begin, t_end);
+				  ray_origin_y += ray_movement;
+				}
+				ray_origin_x += ray_movement;
+			}
+		}
+	}
 
-    BENCHMARK(TraversalOne)->Unit(benchmark::kMillisecond);
-    BENCHMARK(TraversalTwo)->Unit(benchmark::kMillisecond);
-    BENCHMARK(TraversalParallelX)->Unit(benchmark::kMillisecond);
-    BENCHMARK(TraversalParallelY)->Unit(benchmark::kMillisecond);
-    BENCHMARK(TraversalParallelZ)->Unit(benchmark::kMillisecond);
-    BENCHMARK(MultipleRayNoIntersection)->Unit(benchmark::kMillisecond);
-    BENCHMARK(MultipleRayIntersection)->Unit(benchmark::kMillisecond);
-    BENCHMARK(OrthographicRayTrace)->Unit(benchmark::kMillisecond);
+//    BENCHMARK(TraversalOne)->Unit(benchmark::kMillisecond);
+//    BENCHMARK(TraversalTwo)->Unit(benchmark::kMillisecond);
+//    BENCHMARK(TraversalParallelX)->Unit(benchmark::kMillisecond);
+//    BENCHMARK(TraversalParallelY)->Unit(benchmark::kMillisecond);
+//    BENCHMARK(TraversalParallelZ)->Unit(benchmark::kMillisecond);
+//    BENCHMARK(MultipleRayNoIntersection)->Unit(benchmark::kMillisecond);
+//    BENCHMARK(MultipleRayIntersection)->Unit(benchmark::kMillisecond);
+	BENCHMARK(OrthographicOneFPS)->Unit(benchmark::kMillisecond);
 
 } // namespace
 
