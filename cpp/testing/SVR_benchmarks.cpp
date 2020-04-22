bad6dfe6d9cb293df9a98da161572d64d49acb73
Chris Gyurgyik <gyurgyikcp@gmail.com>
2020-04-22 19:02:49 -0400

    Update benchmarks.

--- cpp/testing/SVR_benchmarks.cpp
+++ cpp/testing/SVR_benchmarks.cpp
@@ -143,24 +143,25 @@ namespace {
     // 128^3 domain with 256^2 rays for a scratch paper benchmark.
     // In this case, we can imagine the rays form a square projection
     // on the sphere, and thus not all rays will intersect.
+    // Rays are perpendicular to the XY Plane.
     static void OrthographicRayTracing(benchmark::State &state) {
         for (auto _ : state) {
+            const std::size_t voxel_count = 128;
             const BoundVec3 min_bound(-20000.0, -20000.0, -20000.0);
             const BoundVec3 max_bound(20000.0, 20000.0, 20000.0);
             const BoundVec3 sphere_center(0.0, 0.0, 0.0);
             const double sphere_max_radius = 1000.0;
-            const std::size_t num_radial_sections = 128;
-            const std::size_t num_angular_sections = 128;
-            const std::size_t num_azimuthal_sections = 128;
-            const svr::SphericalVoxelGrid grid(min_bound, max_bound, num_radial_sections,
-                                               num_angular_sections, num_azimuthal_sections,
-                                               sphere_center, sphere_max_radius);
+            const std::size_t num_radial_sections = voxel_count;
+            const std::size_t num_angular_sections = voxel_count;
+            const std::size_t num_azimuthal_sections = voxel_count;
+            const svr::SphericalVoxelGrid grid(min_bound, max_bound, num_radial_sections, num_angular_sections,
+                                               num_azimuthal_sections, sphere_center, sphere_max_radius);
             const double t_begin = 0.0;
             const double t_end = 10000.0;
 
             double ray_origin_x = -1000.0;
             double ray_origin_y = -1000.0;
-            const double ray_origin_z = -1100.0;
+            const double ray_origin_z = -sphere_max_radius - 100.0;
             const double ray_movement = 2000.0 / 256.0;
             for (int i = 0; i < 256; ++i) {
                 for (int j = 0; j < 256; ++j) {
@@ -168,7 +169,7 @@ namespace {
                     const FreeVec3 ray_direction(0.0, 0.0, 1.0);
                     const Ray ray(ray_origin, ray_direction);
                     const auto actual_voxels = sphericalCoordinateVoxelTraversal(ray, grid, t_begin, t_end);
-                    ray_origin_y = (j != 255) ? ray_origin_y + ray_movement : -1000.0;
+                    ray_origin_y = (j == 255) ? -1000.0 : ray_origin_y + ray_movement;
                 }
                 ray_origin_x += ray_movement;
             }
@@ -177,18 +178,19 @@ namespace {
 
     // 64^3 domain with 256^2 rays for a scratch paper benchmark.
     // In this case, all rays intersect the sphere.
+    // Rays are perpendicular to the XY Plane.
     static void OrthographicAllRaysIntersect(benchmark::State &state) {
         for (auto _ : state) {
+            const std::size_t voxel_count = 64;
             const BoundVec3 min_bound(-20000.0, -20000.0, -20000.0);
             const BoundVec3 max_bound(20000.0, 20000.0, 20000.0);
             const BoundVec3 sphere_center(0.0, 0.0, 0.0);
             const double sphere_max_radius = 1000.0 * 10.0;
-            const std::size_t num_radial_sections = 64;
-            const std::size_t num_angular_sections = 64;
-            const std::size_t num_azimuthal_sections = 64;
-            const svr::SphericalVoxelGrid grid(min_bound, max_bound, num_radial_sections,
-                                               num_angular_sections, num_azimuthal_sections,
-                                               sphere_center, sphere_max_radius);
+            const std::size_t num_radial_sections = voxel_count;
+            const std::size_t num_angular_sections = voxel_count;
+            const std::size_t num_azimuthal_sections = voxel_count;
+            const svr::SphericalVoxelGrid grid(min_bound, max_bound, num_radial_sections, num_angular_sections,
+                                               num_azimuthal_sections, sphere_center, sphere_max_radius);
             const double t_begin = 0.0;
             const double t_end = 100000.0;
 
@@ -203,7 +205,7 @@ namespace {
                     const Ray ray(ray_origin, ray_direction);
                     const auto actual_voxels = sphericalCoordinateVoxelTraversal(ray, grid, t_begin, t_end);
                     assert(!actual_voxels.empty());
-                    ray_origin_y = (j != 255) ? ray_origin_y + ray_movement : -1000.0;
+                    ray_origin_y = (j == 255) ? -1000.0 : ray_origin_y + ray_movement;
                 }
                 ray_origin_x += ray_movement;
             }
