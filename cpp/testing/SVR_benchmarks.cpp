943e74cb19a7a496bec386c76e1a4f7928725a91
Chris Gyurgyik <gyurgyikcp@gmail.com>
2020-05-03 10:48:19 -0400

    Update algorithm.

--- cpp/testing/SVR_benchmarks.cpp
+++ cpp/testing/SVR_benchmarks.cpp
@@ -18,15 +18,13 @@ namespace {
     // (-1000.0, -1000.0) -> (1000.0, 1000.0) while remaining outside the sphere in the Z plane.
     // Since the maximum sphere radius is 10^4, this ensures all rays will intersect.
     void inline orthographicTraverseXSquaredRaysinYCubedVoxels(const std::size_t X, const std::size_t Y) noexcept {
-        const std::size_t voxel_count = X;
-        const std::size_t ray_count = Y;
         const BoundVec3 min_bound(-20000.0, -20000.0, -20000.0);
         const BoundVec3 max_bound(20000.0, 20000.0, 20000.0);
         const BoundVec3 sphere_center(0.0, 0.0, 0.0);
         const double sphere_max_radius = 10.0 * 1000.0;
-        const std::size_t num_radial_sections = voxel_count;
-        const std::size_t num_angular_sections = voxel_count;
-        const std::size_t num_azimuthal_sections = voxel_count;
+        const std::size_t num_radial_sections = Y;
+        const std::size_t num_angular_sections = Y;
+        const std::size_t num_azimuthal_sections = Y;
         const svr::SphericalVoxelGrid grid(min_bound, max_bound, num_radial_sections, num_angular_sections,
                                            num_azimuthal_sections, sphere_center, sphere_max_radius);
         const double t_begin = 0.0;
@@ -36,15 +34,14 @@ namespace {
         double ray_origin_y = -1000.0;
         const double ray_origin_z = -(sphere_max_radius + 1000.0);
 
-        const double ray_origin_plane_movement = 2000.0 / ray_count;
-        const std::size_t end_of_column = ray_count - 1;
-        for (std::size_t i = 0; i < ray_count; ++i) {
-            for (std::size_t j = 0; j < ray_count; ++j) {
+        const double ray_origin_plane_movement = 2000.0 / X;
+        for (std::size_t i = 0; i < X; ++i) {
+            for (std::size_t j = 0; j < X; ++j) {
                 const BoundVec3 ray_origin(ray_origin_x, ray_origin_y, ray_origin_z);
                 const FreeVec3  ray_direction(0.0, 0.0, 1.0);
                 const auto actual_voxels = sphericalCoordinateVoxelTraversal(Ray(ray_origin, ray_direction),
                                                                              grid, t_begin, t_end);
-                ray_origin_y = (j == end_of_column) ? -1000.0 : ray_origin_y + ray_origin_plane_movement;
+                ray_origin_y = (j == X - 1) ? -1000.0 : ray_origin_y + ray_origin_plane_movement;
             }
             ray_origin_x += ray_origin_plane_movement;
         }
@@ -183,13 +180,13 @@ namespace {
 
     static void Orthographic_128SquaredRays_64CubedVoxels(benchmark::State &state) {
         for (auto _ : state) {
-            orthographicTraverseXSquaredRaysinYCubedVoxels(64, 128);
+            orthographicTraverseXSquaredRaysinYCubedVoxels(128, 64);
         }
     }
 
     static void Orthographic_256SquaredRays_128CubedVoxels(benchmark::State &state) {
         for (auto _ : state) {
-            orthographicTraverseXSquaredRaysinYCubedVoxels(128, 256);
+            orthographicTraverseXSquaredRaysinYCubedVoxels(256, 128);
         }
     }
 
