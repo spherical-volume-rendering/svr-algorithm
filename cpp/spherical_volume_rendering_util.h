54e2a210eebbbc1e3dc11d96c35e8d682cdae658
Chris Gyurgyik <gyurgyikcp@gmail.com>
2020-05-11 11:52:08 -0400

    Update doc.

--- cpp/spherical_volume_rendering_util.h
+++ cpp/spherical_volume_rendering_util.h
@@ -24,11 +24,9 @@ namespace svr {
     };
 
     // A spherical coordinate voxel traversal algorithm. The algorithm traces the ray over the spherical voxel grid
-    // provided. t_begin is the time the ray begins, and t_end is the time at which the ray ends. Returns a
-    // vector of the spherical coordinate voxels traversed. Does not assume that the ray direction is normalized.
-    // Recall that if, for example, a radial hit occurs, the azimuthal and polar voxels will remain the
-    // same as before. This applies for each traversal type. The sphere bounds are used to determine the sector
-    // of the sphere to be traversed.
+    // provided. t_begin is the time the ray begins, and t_end is the time at which the ray ends. It does not
+    // assumes the ray direction is normalized. Returns a vector of the spherical coordinate voxels traversed.
+    // The sphere bounds are used to determine the sector of the sphere to be traversed.
     // For example, if one wants to traverse the entire sphere, then:
     // SphereBound min_bound = { .radial=0.0, .polar=0.0, .azimuthal=0.0 }
     // SphereBound max_bound = { .radial=SPHERE_MAX_RADIUS, .polar=2 * M_PI, .azimuthal=2 * M_PI }
