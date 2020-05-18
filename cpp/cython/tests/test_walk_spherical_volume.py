9a2d1447d383b494ab8c1a406c8662875847a3a1
Chris Gyurgyik <gyurgyikcp@gmail.com>
2020-05-18 18:57:23 -0400

    Add cythonized cases.

--- cpp/cython/tests/test_walk_spherical_volume.py
+++ cpp/cython/tests/test_walk_spherical_volume.py
@@ -589,6 +589,87 @@ class TestWalkSphericalVolume(unittest.TestCase):
         expected_phi_voxels = [2, 2, 2, 2]
         self.verify_voxels(voxels, expected_radial_voxels, expected_theta_voxels, expected_phi_voxels)
 
+    def test_ray_begins_past_sphere_origin_one(self):
+        ray_origin = np.array([0.0, 0.0, 0.0])
+        ray_direction = np.array([-3.0, 2.4, -3.0])
+        sphere_center = np.array([0.0, 0.0, 0.0])
+        sphere_max_radius = 10.0
+        num_radial_sections = 4
+        num_polar_sections = 4
+        num_azimuthal_sections = 4
+        t_begin = 0.0
+        t_end = 30.0
+        min_bound = np.array([0.0, 0.0, 0.0])
+        max_bound = np.array([sphere_max_radius, 2 * np.pi, 2 * np.pi])
+        voxels = cython_SVR.walk_spherical_volume(ray_origin, ray_direction, min_bound, max_bound,
+                                                  num_radial_sections, num_polar_sections, num_azimuthal_sections,
+                                                  sphere_center, t_begin, t_end)
+        expected_radial_voxels = [3, 2, 1]
+        expected_theta_voxels = [1, 1, 1]
+        expected_phi_voxels = [2, 2, 2]
+        self.verify_voxels(voxels, expected_radial_voxels, expected_theta_voxels, expected_phi_voxels)
+
+    def test_ray_begins_past_sphere_origin_two(self):
+        ray_origin = np.array([0.0, 0.0, 0.0])
+        ray_direction = np.array([-4.5, 3.6, -4.5])
+        sphere_center = np.array([0.0, 0.0, 0.0])
+        sphere_max_radius = 10.0
+        num_radial_sections = 4
+        num_polar_sections = 4
+        num_azimuthal_sections = 4
+        t_begin = 0.0
+        t_end = 30.0
+        min_bound = np.array([0.0, 0.0, 0.0])
+        max_bound = np.array([sphere_max_radius, 2 * np.pi, 2 * np.pi])
+        voxels = cython_SVR.walk_spherical_volume(ray_origin, ray_direction, min_bound, max_bound,
+                                                  num_radial_sections, num_polar_sections, num_azimuthal_sections,
+                                                  sphere_center, t_begin, t_end)
+        expected_radial_voxels = [2, 1]
+        expected_theta_voxels = [1, 1]
+        expected_phi_voxels = [2, 2]
+        self.verify_voxels(voxels, expected_radial_voxels, expected_theta_voxels, expected_phi_voxels)
+
+    def test_ray_begins_past_sphere_origin_three(self):
+        ray_origin = np.array([0.0, 0.0, 0.0])
+        ray_direction = np.array([-6.0, 4.8, -6.0])
+        sphere_center = np.array([0.0, 0.0, 0.0])
+        sphere_max_radius = 10.0
+        num_radial_sections = 4
+        num_polar_sections = 4
+        num_azimuthal_sections = 4
+        t_begin = 0.0
+        t_end = 30.0
+        min_bound = np.array([0.0, 0.0, 0.0])
+        max_bound = np.array([sphere_max_radius, 2 * np.pi, 2 * np.pi])
+        voxels = cython_SVR.walk_spherical_volume(ray_origin, ray_direction, min_bound, max_bound,
+                                                  num_radial_sections, num_polar_sections, num_azimuthal_sections,
+                                                  sphere_center, t_begin, t_end)
+        expected_radial_voxels = [1]
+        expected_theta_voxels = [1]
+        expected_phi_voxels = [2]
+        self.verify_voxels(voxels, expected_radial_voxels, expected_theta_voxels, expected_phi_voxels)
+
+    def test_ray_begins_past_sphere_origin_four(self):
+        ray_origin = np.array([0.0, 0.0, 0.0])
+        ray_direction = np.array([-7.5, 6.0, -7.5])
+        sphere_center = np.array([0.0, 0.0, 0.0])
+        sphere_max_radius = 10.0
+        num_radial_sections = 4
+        num_polar_sections = 4
+        num_azimuthal_sections = 4
+        t_begin = 0.0
+        t_end = 30.0
+        min_bound = np.array([0.0, 0.0, 0.0])
+        max_bound = np.array([sphere_max_radius, 2 * np.pi, 2 * np.pi])
+        voxels = cython_SVR.walk_spherical_volume(ray_origin, ray_direction, min_bound, max_bound,
+                                                  num_radial_sections, num_polar_sections, num_azimuthal_sections,
+                                                  sphere_center, t_begin, t_end)
+        expected_radial_voxels = []
+        expected_theta_voxels = []
+        expected_phi_voxels = []
+        self.verify_voxels(voxels, expected_radial_voxels, expected_theta_voxels, expected_phi_voxels)
+
+
     def test_ray_tangential_hit(self):
         ray_origin = np.array([-5.0, 0.0, 10.0])
         ray_direction = np.array([0.0, 0.0, -1.0])
